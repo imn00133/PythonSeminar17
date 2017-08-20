979edb484b874d261ec50c6d7e467cdc5b38515e
KJH <imn00133@gmail.com>
2017-08-20 20:02:37 +0900

    Junho assingment submit

---
+++ exercise/369/Junho/08_21_Junho_369.py
@@ -0,0 +1,9 @@
+﻿print("아무 숫자나 입력해 보세욤~")
+UserChoice=int(input())
+for Num in [str(index) for index in range(1, UserChoice+1)]:
+    times1 = Num.count('3') + Num.count('6') + Num.count('9')
+    times2 = Num.count('2') + Num.count('4') + Num.count('6') + Num.count('8')
+    if times1 or times2:
+        print("뽕!" * times2 + "짝!" * times1)
+    else:
+        print(Num)
---
+++ exercise/montyhall/Junho/08_21_montyhall.py
@@ -0,0 +1,25 @@
+﻿import random
+trial=0
+while trial<=100000:
+    win=0
+    doorcount=int(input("Coose the Number of Doors \n"))
+    if doorcount>1:
+        Choice=random.randint(1,doorcount+1)
+        Answer=random.randint(1,doorcount+1)
+        if Choice==Answer:
+            print("Success")
+            trial+=1
+            win+=1
+        else:
+            print("Fail")
+            trial+=1
+    else:
+        print("2 이상의 숫자를 입력하십시오")
+        break
+    print(win/trial)
+    print("%d번째 시도" %trial)
+
+
+#바꾸지 않을 경우를 가정함
+#따라서 Choice도 랜덤, Answer도 랜덤일 때 choice=random 이면 success
+#문제는 한번 끝나고 doorcount를 설정하는 곳으로 되돌아감 
