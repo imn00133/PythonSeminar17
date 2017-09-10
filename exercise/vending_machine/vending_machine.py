1e77fd592b8413de1b8d18231eded807d7e90db3
KJH <imn00133@gmail.com>
2017-09-10 19:37:06 +0900

    vending_machine

--- exercise/vending_machine/vending_machine.py
+++ exercise/vending_machine/vending_machine.py
@@ -1,4 +1,29 @@
-money = int(input("돈을 넣으세요: "))
+def read_stock():
+    coffee_stock = open("coffee_stock.txt", 'r', encoding="utf-8")
+    coffee_list = []
+    while True:
+        line = coffee_stock.readline()
+        if not line:
+            break
+        coffee_list.append(line.strip().split())
+    return coffee_list
+
+
+def write_stock(coffee_list):
+    coffee_stock = open("coffee_stock.txt", 'w', encoding="utf-8")
+
+
+def admin_mode():
+    coffee_list = read_stock()
+    for i in range(1, len(coffee_list)+1):
+        print("%d. %s: %d개" % (coffee_list[i][0], coffee_list[i][1], coffee_list[i][3]))
+
+
+while True:
+    money = input("돈을 넣으세요: ")
+    if money == "admin":
+        admin_mode()
+
 # 사전 사용해보기
 coffee_dict = {1: "블랙커피", 2: "밀크커피", 3: "고급커피", 4: "거스름돈"}
 coffee_value = {"블랙커피": 100, "밀크커피": 150, "고급커피": 250}
