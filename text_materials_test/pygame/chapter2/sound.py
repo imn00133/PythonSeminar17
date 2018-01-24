e73d62f82d12a6dd80733d380b1ef4b26b073281
Kim Jaehyeong <imn00133@gmail.com>
2018-01-25 00:24:14 +0900

    revise sound.py to play backgoround music

--- text_materials_test/pygame/chapter2/sound.py
+++ text_materials_test/pygame/chapter2/sound.py
@@ -6,12 +6,17 @@ from pygame.locals import *
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Sound')
+soundObj = pygame.mixer.Sound("beep1.ogg")
+pygame.mixer.music.load("Shining.mp3")
+pygame.mixer.music.play(-1, 0.0)
 
 while True: # main game loop
-    soundObj = pygame.mixer.Sound("beep1.ogg")
     soundObj.play()
+    pygame.mixer.music.play(-1, 0.0)
     time.sleep(1)  # 1초 동안 소리를 재생하도록 둔다.
     soundObj.stop()
+    time.sleep(10)
+    pygame.mixer.music.stop()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
