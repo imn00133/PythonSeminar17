4fe30ebf7dbaaf266d330a866e3d531c696f0c66
Kim Jaehyeong <imn00133@gmail.com>
2018-02-05 15:00:09 +0900

    make background move add background and background move in airplane game

---
+++ text_materials_test/pygame/airplane_game/3_background.py
@@ -0,0 +1,124 @@
+# https://blog.naver.com/PostList.nhn?from=postList&blogId=samsjang&categoryNo=80&currentPage=10
+# revision Jaehyeong Kim
+
+import pygame
+import sys
+from pygame.locals import *
+
+# 상수영역
+# 초당 프레임 수
+FPS = 30
+# 윈도우 크기, 비율 일정하게 만듦
+WINDOWWIDTH = 1080
+WINDOWHEIGHT = int(WINDOWWIDTH / 2)
+# 배경 최대 크기
+ORIGINBACKGROUNDWIDTH = 1280
+ORIGINBACKGROUNDHEIGHT = 640
+# 배경 움직임 속도
+BACKGROUNDSPEED = 2
+# 색
+WHITE = (255, 255, 255)
+
+
+def draw_object(image, x, y):
+    global DISPLAYSURF
+    DISPLAYSURF.blit(image, (x, y))
+
+
+def main():
+    global FPSCLOCK, DISPLAYSURF
+    global IMAGESDICT
+
+    # 비행기 왼쪽 초기 위치
+    airplane_x = WINDOWWIDTH * 0.05
+    airplane_y = WINDOWHEIGHT * 0.8
+    airplane_y_change = 0
+    airplane_x_change = 0
+
+    # 비행기 크기
+    AIRPLANEWIDTH = IMAGESDICT["airplane"].get_width()
+    AIRPLANEHEIGHT = IMAGESDICT["airplane"].get_height()
+
+    # 윈도우 변경에 따른 배경크기 변경
+    BACKGROUNDWIDTH = IMAGESDICT["background"].get_width()
+
+    # 배경 초기 위치
+    background_x = 0
+    other_background_x = BACKGROUNDWIDTH
+
+    # game loop
+    while True:
+        # event handle
+        for event in pygame.event.get():
+            # 종료
+            if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    airplane_y_change = -5
+                elif event.key == K_DOWN:
+                    airplane_y_change = 5
+                elif event.key == K_RIGHT:
+                    airplane_x_change = 5
+                elif event.key == K_LEFT:
+                    airplane_x_change = -5
+            if event.type == KEYUP:
+                if event.key == K_UP or event.key == K_DOWN:
+                    airplane_y_change = 0
+                elif event.key == K_RIGHT or event.key == K_LEFT:
+                    airplane_x_change = 0
+
+        # event에 따른 비행기 위치 변경 및 제한
+        airplane_y += airplane_y_change
+        if airplane_y < 0:
+            airplane_y = 0
+        elif airplane_y > WINDOWHEIGHT - AIRPLANEHEIGHT:
+            airplane_y = WINDOWHEIGHT - AIRPLANEHEIGHT
+
+        airplane_x += airplane_x_change
+        if airplane_x < 0:
+            airplane_x = 0
+        elif airplane_x > WINDOWWIDTH - AIRPLANEWIDTH:
+            airplane_x = WINDOWWIDTH - AIRPLANEWIDTH
+
+        # 배경 그리기
+        background_x -= BACKGROUNDSPEED
+        if background_x == -BACKGROUNDWIDTH:
+            background_x = BACKGROUNDWIDTH
+        draw_object(IMAGESDICT["background"], background_x, 0)
+
+        other_background_x -= BACKGROUNDSPEED
+        if other_background_x == -BACKGROUNDWIDTH:
+            other_background_x = BACKGROUNDWIDTH
+        draw_object(IMAGESDICT["background"], other_background_x, 0)
+
+        # 다른 스프라이트 그리기
+        draw_object(IMAGESDICT["airplane"], airplane_x, airplane_y)
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+
+def game_init():
+    global FPSCLOCK, DISPLAYSURF
+    global IMAGESDICT
+    FPSCLOCK = pygame.time.Clock()
+    pygame.init()
+
+    # DISPLAY Surface 설정하기
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('PyFlying')
+
+    # 이미지 받아오기
+    IMAGESDICT = {"airplane": pygame.image.load('images/plane.png'),
+                  "background": pygame.image.load('images/background.png')}
+
+    # 배경 이미지 게임 윈도우 크기에 맞추기
+    assert WINDOWWIDTH <= ORIGINBACKGROUNDWIDTH or WINDOWHEIGHT <= ORIGINBACKGROUNDHEIGHT,\
+        '게임 윈도우 크기가 너무 큽니다.'
+    IMAGESDICT["background"] = pygame.transform.scale(IMAGESDICT["background"], (WINDOWWIDTH, WINDOWHEIGHT))
+    main()
+
+
+if __name__ == '__main__':
+    game_init()
